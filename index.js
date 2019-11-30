de06ed0e0cacc3c80c1fd6035bd5067beab26bc9
Ryan Lee <ryanwoolee@gmail.com>
2019-11-30 02:21:02 -0700

    error handling

--- index.js
+++ index.js
@@ -5,31 +5,42 @@ const routes = require("./routes/api");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
+var timeout = require("connect-timeout");
 
 // settings
 const portNumber = process.env.DB_PORT || 4000;
 
 // set up express app
 const app = express();
+app.use(timeout("10s"));
 
 // connect to mongodb
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useUnifiedTopology", true);
 mongoose.connect(
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}`
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}`,
+  function(err) {
+    if (err) throw err;
+  }
 );
 mongoose.Promise = global.Promise;
 
 app.use(cors({ origin: "*", credentials: true }));
-
 app.use(bodyParser.json());
+app.use(haltOnTimedout);
 
 app.use("/api", routes);
 
 // error handling middleware
 app.use(function(err, req, res, next) {
+  console.log(err);
   res.status(422).send({ error: err.message });
 });
+
+function haltOnTimedout(req, res, next) {
+  if (!req.timedout) next();
+  console.log("timeout");
+}
 
 // listen for request
 app.listen(portNumber, function() {
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const Task = require("../models/Task");
+var timeout = require("express-timeout-handler");
 
 // get a list of tasks from the db
 router.get("/tasks", function(req, res, next) {
+  console.log("get request");
+
   Task.find({})
     .then(function(tasks) {
       console.log(tasks);
@@ -14,7 +17,8 @@ router.get("/tasks", function(req, res, next) {
 
 // add a new content to the db
 router.post("/tasks", function(req, res, next) {
-  console.log(req.body);
+  console.log("post request");
+
   Task.create(req.body)
     .then(function(task) {
       res.send(task);
@@ -24,6 +28,8 @@ router.post("/tasks", function(req, res, next) {
 
 // update content in the db
 router.put("/tasks/:id", function(req, res, next) {
+  console.log("put request");
+
   Task.findByIdAndUpdate({ _id: req.params.id }, req.body).then(function() {
     Task.findOne({ _id: req.params.id }).then(function(task) {
       res.send(task);
@@ -33,6 +39,8 @@ router.put("/tasks/:id", function(req, res, next) {
 
 // delete content from the db
 router.delete("/tasks/:id", function(req, res, next) {
+  console.log("delete request");
+
   Task.findByIdAndRemove({ _id: req.params.id }).then(function(task) {
     res.send(task);
   });
